27249a859003e5794313dac8571359823702a19b
Legitbub <153683076+Legitbub@users.noreply.github.com>
2024-09-18 21:59:52 -0700

    Hotel class begin

---
+++ Hotel.java
@@ -0,0 +1,11 @@
+/*import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;*/
+
+public class Hotel {
+    /*private String name;
+    private List<Guest> guestList = new ArrayList<>();
+    private List<Employee> employeeList = new ArrayList<>();
+    private HashMap<Room, Guest> roomLog = new HashMap<Room, Guest>();
+    private HashMap<Amenity, Guest> amenityLog = new HashMap<Amenity, Guest>();*/
+}
