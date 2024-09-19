89bbf28ca4b024add61fa22e193fd693b9a55c71
Legitbub <153683076+Legitbub@users.noreply.github.com>
2024-09-18 22:09:07 -0700

    Hotel class begin

--- Hotel.java
+++ Hotel.java
@@ -1,11 +1,40 @@
 /*import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;*/
+import java.util.List;
+*/
 
 public class Hotel {
     /*private String name;
     private List<Guest> guestList = new ArrayList<>();
     private List<Employee> employeeList = new ArrayList<>();
     private HashMap<Room, Guest> roomLog = new HashMap<Room, Guest>();
-    private HashMap<Amenity, Guest> amenityLog = new HashMap<Amenity, Guest>();*/
+    private HashMap<Amenity, Guest> amenityLog = new HashMap<Amenity, Guest>();
+
+    public Hotel() {
+        name = "Default Hotel";
+    }
+
+    public Hotel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Guest> getGuestList() {
+        return guestList;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public HashMap<Room, Guest> getRoomLog() {
+        return roomLog;
+    }
+
+    public HashMap<Amenity, Guest> getAmenityLog() {
+        return amenityLog;
+    }*/
 }
