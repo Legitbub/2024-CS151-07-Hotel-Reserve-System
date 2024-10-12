1459f23ba23495392b41986cf0ce66a9ae86a679
ObviousYordle <michaeltran0925@gmail.com>
2024-10-11 18:26:51 -0700

    Added list of employee that Manager can access

--- Manager.java
+++ Manager.java
@@ -1,28 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager extends Employee{
 
     private Hotel hotel;
+    private List<Employee> employeeList;
     public Manager(String ID) {
         super(ID);
     }
 
-    public Manager(String ID, String name, String position, double wage) {
+    public Manager(String ID, String name, String position, double wage, List<Employee> employeeList) {
         super(ID, name, position, wage);
+        //Manager will have access to employeeList
+        this.employeeList = employeeList;
     }
 
     //Fire an employee and remove from the list of employee;
     public void fireEmployee(String employeeID){
         if(hotel != null){
-            boolean employeeFound = false;
-
-            for(Employee employee : hotel.getEmployeeList()){
-                if(employee.getId().equals(employeeID)){
-                    hotel.getEmployeeList().remove(employee);
-                    System.out.printf("%s has been fired!", employee.getName());
-                    employeeFound = true;
-                }
-            }
+            //removeIf will prevent CoccurentModification Error
+            boolean employeeFound = hotel.getEmployeeList().removeIf(employee -> employee.getId().equals(employeeID));
+            
             if(!employeeFound){
                 System.out.println("There is no employee with the provided ID in the system");
+            }
+            else{
+                System.out.printf("%s has been fired!", employeeID);
+
             }
         }
         else{
@@ -34,7 +38,7 @@ public class Manager extends Employee{
 
     }
     //Change wages + hours;
-    public void modifyEmployeeDetails(){
+    public void modifyEmployeeDetails(String employeeID){
 
     }
     //Get a general date and pay all
@@ -44,7 +48,8 @@ public class Manager extends Employee{
             double totalPaymentAfterTax = employeePayment.calculateTaxes();
 
             employeePayment.processPayment(totalPaymentAfterTax);
-
+            //reset hour back to 0
+            employee.setHoursWorked(0);
             System.out.println("Payment of $" + totalPaymentAfterTax + " made to " + employee.getName());
 
         }
