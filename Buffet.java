ad2f5c5469a733e9b3b1ba2e43a484376edf5403
Joshua Mack <joshjoshmack@gmail.com>
2024-10-16 19:17:04 -0700

    added Amenity methods, subclasses

--- Buffet.java
+++ Buffet.java
@@ -1,32 +1,49 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Buffet extends Amenity implements Reservable{
     private ArrayList<String> menu = new ArrayList<>();
     private ArrayList<ArrayList<Guest>> parties = new ArrayList<>();
-    private enum MenuType{BREAKFAST, LUNCH, DINNER}
+    private String menuType;
 
-    MenuType menuType;
-    
-    public Buffet(){
-        super();
-    };
-
-    public Buffet(String name, String description, int price, MenuType menuType){
-        super(name, description, price);
+    public Buffet(String name, String description, float basePrice, String menuType){
+        super(name, description, basePrice);
         this.menuType = menuType;
     }
 
-        
+    public String getMenuType(){
+        return menuType;
+    }
+
+    public void setMenuType(String menuType){
+        this.menuType = menuType;
+    }
+
+    public ArrayList<ArrayList<Guest>> getParties(){
+        return parties;
+    }        
 
     public void showMenu(){
         System.out.println(menu.toString());
+    }
+
+    @Override
+    public boolean reserve(Guest g){
+        if(super.reserve(g)){
+            parties.add(new ArrayList<>(List.of(g)));
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public boolean reserveParty(ArrayList<Guest> party){
         if(occupants.size() + party.size() <= maxOccupancy){
             for(Guest g : party){
                 occupants.add(g);
+                totalReservations++;
             }
+            parties.add(party);
             if(occupants.size() == maxOccupancy)
                 isAvailable = false;
             return true;
--- Gym.java
+++ Gym.java
@@ -1,7 +1,39 @@
-public class Gym extends Amenity {
-    public Gym(){};
+import java.util.HashMap;
 
-    public Gym(String name, String description, int price){
-        super(name, description, price);
+public class Gym extends Amenity implements Reservable{
+    private HashMap<Guest,Employee> personalTrainingSessions = new HashMap<>();
+
+    public Gym(String name, String description, float basePrice){
+        super(name, description, basePrice);
+    }
+
+    public HashMap<Guest,Employee> getPersonalTrainingSessions(){
+        return personalTrainingSessions;
+    }
+
+    @Override
+    public boolean reserve(Guest g){
+        if(super.reserve(g)){
+            System.out.println("If you would like, you can now request a personal trainer!");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean reservePersonalTrainer(Guest g, Employee e){
+        if(!occupants.contains(g)){
+            System.out.println("Please make a gym reservation before reserving a personal trainer");
+            return false;
+        }else if (personalTrainingSessions.containsKey(g)) {
+            System.out.println("Guest " + g.getName() + " already has a personal trainer reserved.");
+            return false;
+        }else if (personalTrainingSessions.containsValue(e)){
+            System.out.println("Employee + " + e.getName() + " is already assigned to another guest");
+            return false;
+        }else{
+            personalTrainingSessions.put(g, e);
+            System.out.println("Successfully reserved perssonal trainer.");
+            return true;
+        }
     }
 }
